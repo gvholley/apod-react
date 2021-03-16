e23cb651360ba129c697423700398327797c4921
Grant Holley <gvholley@gmail.com>
2021-03-16 21:37:12 +0900

    Added API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/navbar';
-import createDOMNodes from './components/card'
 import './App.css';
 
-//UI variables
+//Selects HTML elements
 const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
@@ -11,7 +10,7 @@ const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
 //NASA API
-const count = 9;
+const count = 6;
 const apiKey = 'Qx2dxWtEzDNgGAZ0MQdXyZZwjkBMctbDhzt6ACNd'
 const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
@@ -19,17 +18,53 @@ let resultsArray =[];
 //Local Storage
 let favorites = {};
 
-//Shows correct navbar
-function showContent(page) {
-  window.scrollTo({ top: 0, behavior: 'instant' });
-  loader.classList.add('hidden');
-  if (page === 'favorites') {
-    resultsNav.classList.remove('hidden');
-    favoritesNav.classList.add('hidden');
+
+
+//Creates card
+function createDOMNodes(page) {
+  const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+
+  currentArray.forEach((result) => {
+    const card = document.createElement('div');
+    card.classList.add('card')
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'NASA APOD';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+    if (page === 'results') {
+      saveText.textContent = 'Add to Favorites';
+      saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
   } else {
-    resultsNav.classList.add('hidden');
-    favoritesNav.classList.remove('hidden');
+      saveText.textContent = 'Remove from Favorites';
+      saveText.setAttribute('onclick', `removeFavorite('${result.url}')`);
   }
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+    const copyright = document.createElement('span');
+    copyright.textContent = `${copyrightResult}`;
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+  });
 }
 
 //Updates the DOM
@@ -42,14 +77,12 @@ function updateDOM(page) {
   //Removes elements appended to container
   imagesContainer.textContent = '';
   createDOMNodes(page)
-  showContent()
 }
 
 // Get Images from NASA API
 
 async function getNasaPictures() {
   //Show Loader
-  loader.classList.remove('hidden');
   try {
     const response = await fetch(apiURL);
     resultsArray = await response.json();
@@ -85,6 +118,7 @@ function removeFavorite(itemUrl) {
     updateDOM('favorites')
   }
 }
+
 //On Load
 getNasaPictures();
 
@@ -93,7 +127,6 @@ function App() {
     <div className="container">
     <Navbar />
     <div className="images-container">
-    <createDOMNodes />
     </div>
     <div className="save-confirmed" hidden>
       <h1>ADDED!</h1>
