3134ef317a34fe06bcc3dbf3472b09c1c84d12b6
Grant Holley <gvholley@gmail.com>
2021-03-17 20:09:12 +0900

    Reset JSON

--- src/App.js
+++ src/App.js
@@ -4,68 +4,19 @@ import Card from './components/card';
 import Navbar from './components/navbar';
 import './App.css';
 
-const count = 6;
-const apiKey = 'Qx2dxWtEzDNgGAZ0MQdXyZZwjkBMctbDhzt6ACNd'
-const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
-
-const App = () => {
-  const [photoData, setPhotoData] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-
-
-//Moved useEffect here from search
-  useEffect(() => {
-    const getPhotos = async () => {
-    const response = await fetch(apiURL);
-    const responseJson = await response.json();
-
-    if(responseJson.Search) {
-      setPhotoData(responseJson.Search);
-    }
-  };
-    getPhotos();
-  }, []);
-
-
-  useEffect(()=> {
-
-      const photoFavorites = JSON.parse(
-        localStorage.getItem('app-favorites')
-        );
-      if (photoFavorites) {
-        setFavorites(photoFavorites);
-      }
-  }, []);
-
-
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem('app-favorites', JSON.stringify(items));
-  };
-
-  const addFavoritePhoto = (photo) => {
-    const newFavoriteList = [...favorites, photo];
-    setFavorites(newFavoriteList);
-    saveToLocalStorage(newFavoriteList);
-  };
-
-
-  const removeFavoriteMovie = (photo) => {
-    const newFavoriteList = favorites.filter(
-      (favorite) => favorite.date !== photo.date
-      );
-
-
-    setFavorites(newFavoriteList);
-    saveToLocalStorage(newFavoriteList);
-  };
-
+function App() {
   return (
-    <div className='container'>
-        <Navbar />
-        <Card />
-      </div>
+    <div className="container">
+    <Navbar />
+    <div className="images-container">
+    <Card />
+    </div>
+    <div className="save-confirmed" hidden>
+      <h1>ADDED!</h1>
+    </div>
+    </div>
   );
-};
+}
 
 export default App;
