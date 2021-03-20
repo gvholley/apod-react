861f74233f8d05c6025d78ca2ae4bd929fc39762
Grant Holley <gvholley@gmail.com>
2021-03-20 20:34:31 +0900

    Added Loader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import Card from './components/card';
 import Navbar from './components/navbar';
+import Loader from  './components/loader';
 import './App.css';
+import '.earth.svg';
+
+
 
 
 function App() {
   return (
     <div className="container">
     <Navbar />
+    <Loader />
     <Card />
     <div className="save-confirmed" hidden>
       <h1>ADDED!</h1>
