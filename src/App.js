d9b76fdd05ff8c83549593b33ae8f8728a528e1f
Grant Holley <gvholley@gmail.com>
2021-03-22 12:55:07 +0900

    Edited loader again

--- src/App.js
+++ src/App.js
@@ -11,17 +11,6 @@ const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
-function showContent(page) {
-  window.scrollTo({ top: 0, behavior: 'instant' });
-  loader.classList.add('hidden');
-  if (page === 'favorites') {
-    resultsNav.classList.remove('hidden');
-    favoritesNav.classList.add('hidden');
-  } else {
-    resultsNav.classList.add('hidden');
-    favoritesNav.classList.remove('hidden');
-  }
-}
 
 function removeLoader() {
   loader.classList.remove('hidden');
--- src/components/loader.js
+++ src/components/loader.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import earth from './earth.svg';
 
+const loader = document.querySelector('.loader');
+
+ setTimeout(function(){
+            loader.classList.add('hidden');
+    }, 3000);
+
 
 function Loader() {
   return (
-    <div className="loader hidden">
+    <div className="loader">
       <img src= {earth} alt="earth-loader" />
     </div>
+
   )
 }
 
